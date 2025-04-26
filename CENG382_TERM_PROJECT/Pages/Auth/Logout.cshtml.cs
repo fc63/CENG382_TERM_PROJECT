80c1f8939a28cebd432d6ae2d32e9939997f05dc
fc63 <furkan@coban.email>
2025-04-26 13:12:39 +0300

    csrf

--- CENG382_TERM_PROJECT/Pages/Auth/Logout.cshtml.cs
+++ CENG382_TERM_PROJECT/Pages/Auth/Logout.cshtml.cs
@@ -1,19 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace CENG382_TERM_PROJECT.Pages.Auth
 {
     public class LogoutModel : PageModel
     {
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-			HttpContext.Session.Clear();
-			Response.Cookies.Delete("username");
-			Response.Cookies.Delete("token");
-			Response.Cookies.Delete("session_id");
-			await HttpContext.SignOutAsync();
-			return RedirectToPage("/Auth/Login");
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear();
+            Response.Cookies.Delete("username");
+            Response.Cookies.Delete("token");
+            Response.Cookies.Delete("session_id");
+            return RedirectToPage("/Auth/Login");
         }
     }
 }
