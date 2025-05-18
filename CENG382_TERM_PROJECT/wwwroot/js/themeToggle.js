6ec76e499ac214989203e62713cef2610a63554a
fc63 <furkan@coban.email>
2025-05-18 14:15:26 +0300

    v0.777 dark mode toggle

---
+++ CENG382_TERM_PROJECT/wwwroot/js/themeToggle.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    const toggleButton = document.getElementById("themeToggle");
+    if (toggleButton) {
+        toggleButton.addEventListener("click", () => {
+            const newTheme = document.documentElement.getAttribute("data-theme") === "light" ? "dark" : "light";
+            document.documentElement.setAttribute("data-theme", newTheme);
+            localStorage.setItem("theme", newTheme);
+        });
+    }
+});
